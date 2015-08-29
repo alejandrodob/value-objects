02b2efdd40037448c6702ee9ae42f5fa08c97a71
alejandrodob <alejandrodob@gmail.com>
2015-08-29 13:32:07 +0200

    Check fields have been declared when instance creation

--- spec/value_object_spec.py
+++ spec/value_object_spec.py
@@ -2,7 +2,7 @@ import value_object
 
 class Point(object):
     __metaclass__ = value_object.ValueObject
-
+    __fields__ = ('x', 'y')
 
 with description(value_object.ValueObject):
 
@@ -10,8 +10,16 @@ with description(value_object.ValueObject):
 
         with context('with keyword arguments in constructor'):
 
-            with it('generates constructor, fields and accessors for declared fields'):
+            with it('generates constructor and fields for declared fields'):
                 a_value_object = Point(x=5, y=3)
 
                 assert a_value_object.x == 5
                 assert a_value_object.y == 3
+
+            with it('raises an exception for non declared fields'):
+                try:
+                    a_value_object = Point(not_a_field="whatever")
+                except ValueError, e:
+                    assert True
+                else:
+                    assert False
--- value_object.py
+++ value_object.py
@@ -3,6 +3,8 @@ class ValueObject(type):
     def __call__(self, *args, **kwargs):
         obj = type.__call__(self, *args)
         for field in kwargs:
+            if field not in self.__fields__:
+                raise ValueError
             setattr(obj, field, kwargs[field])
         return obj
 
