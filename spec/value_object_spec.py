666ac6c44189a623c48d5fd176c4135216289a8b
alejandrodob <alejandrodob@gmail.com>
2015-09-04 23:48:32 +0200

    Refactor: use expects matchers

--- spec/value_object_spec.py
+++ spec/value_object_spec.py
@@ -1,87 +1,68 @@
 import value_object
+from expects import *
+
 
 class Point(object):
     __metaclass__ = value_object.ValueObject
     __fields__ = ('x', 'y')
 
+
 with description(value_object.ValueObject):
 
     with description("standard behaviour"):
-
         with context('with keyword arguments in constructor'):
-
             with it('generates instance attributes for declared fields'):
                 a_value_object = Point(x=5, y=3)
 
-                assert a_value_object.x == 5
-                assert a_value_object.y == 3
+                expect(a_value_object.x).to(equal(5))
+                expect(a_value_object.y).to(equal(3))
 
             with it('raises an exception for non declared fields'):
-                try:
-                    a_value_object = Point(not_a_field="whatever")
-                except ValueError, e:
-                    assert True
-                else:
-                    assert False, "Exception not raised"
+                def create_point_with_invalid_field():
+                    Point(not_a_field="whatever")
+
+                expect(create_point_with_invalid_field).to(raise_error(ValueError))
 
         with context('with non-keyword arguments in constructor'):
-
             with it('generates instance attributes for declared fields'):
                 a_value_object = Point(5, 3)
 
-                assert a_value_object.x == 5
-                assert a_value_object.y == 3
+                expect(a_value_object.x).to(equal(5))
+                expect(a_value_object.y).to(equal(3))
 
         with context("with mixed arguments and keyword arguments"):
-
             with it('generates instance attributes for declared fields'):
                 a_value_object = Point(5, y=3)
 
-                assert a_value_object.x == 5
-                assert a_value_object.y == 3
+                expect(a_value_object.x).to(equal(5))
+                expect(a_value_object.y).to(equal(3))
 
     with description("restrictions"):
-
         with context('on initialization'):
-
             with it("must at least have one field"):
-                try:
-                    class NoFields(object):
-                        __metaclass__ = value_object.ValueObject
-                        __fields__ = ()
-                    NoFields()
-                except ValueError, e:
-                    assert True
-                else:
-                    assert False, "Exception not raised"
+                class NoFields(object):
+                    __metaclass__ = value_object.ValueObject
+                    __fields__ = ()
+
+                expect(NoFields).to(raise_error(ValueError))
 
             with it("must not have any field initialized to None"):
-                try:
+                def create_point_with_None_argument():
                     Point(None, 3)
-                except ValueError, e:
-                    assert True
-                else:
-                    assert False
+
+                expect(create_point_with_None_argument).to(raise_error(ValueError))
 
             with it('must have number of values equal to number of fields'):
-                try:
-                    a_value_object = Point(5, 3, 4)
-                except ValueError, e:
-                    assert True
-                else:
-                    assert False, "Exception not raised"
+                def create_point_with_too_many_values():
+                    Point(5, 3, 4)
+                def create_point_with_not_enough_values():
+                    Point(5)
 
-                try:
-                    a_value_object = Point(5)
-                except ValueError, e:
-                    assert True
-                else:
-                    assert False, "Exception not raised"
+                expect(create_point_with_too_many_values).to(raise_error(ValueError))
+                expect(create_point_with_not_enough_values).to(raise_error(ValueError))
 
             with _it("must respect order of declared fields"):
-                try:
-                    a_value_object = Point(3, x=5)
-                except ValueError, e:
-                    assert True
-                else:
-                    assert False, "Exception not raised"
+                def create_point_with_invalid_fields_order():
+                    Point(3, x=5)
+
+                expect(create_point_with_invalid_fields_order).to(raise_error(ValueError))
