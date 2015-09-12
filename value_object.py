5511cbc0e619bd790d89cb6b734023ad6ddc18c8
alejandrodob <alejandrodob@gmail.com>
2015-09-12 18:41:08 +0200

    extract method

--- value_object.py
+++ value_object.py
@@ -1,14 +1,9 @@
 class ValueObject(type):
 
     def __call__(self, *args, **kwargs):
-        self._check_fields_declared()
 
-        if None in args:
-            field = self.__fields__[args.index(None)]
-            raise FieldWithoutValue("Declared field '%s' must have a value" % field)
-        none_keyword_args = [k for k, v in kwargs.iteritems() if v == None]
-        if none_keyword_args:
-            raise FieldWithoutValue("Declared field '%s' must have a value" % none_keyword_args[0])
+        self._check_fields_declared()
+        self._check_fields_have_value(*args, **kwargs)
 
         total_values_provided = len(args) + len(kwargs)
         if total_values_provided != len(self.__fields__):
@@ -38,6 +33,14 @@ class ValueObject(type):
     def _check_fields_declared(self):
         if not self.__fields__:
             raise NoFieldsDeclared()
+
+    def _check_fields_have_value(self, *args, **kwargs):
+        if None in args:
+            field = self.__fields__[args.index(None)]
+            raise FieldWithoutValue("Declared field '%s' must have a value" % field)
+        none_keyword_args = [k for k, v in kwargs.iteritems() if v == None]
+        if none_keyword_args:
+            raise FieldWithoutValue("Declared field '%s' must have a value" % none_keyword_args[0])
 
 
 class WrongField(Exception):
