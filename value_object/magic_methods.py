e5479d10357cc836fab0214adf06d7fdd27c56ff
alejandrodob <alejandrodob@gmail.com>
2015-10-10 00:13:54 +0200

    Extract module for magic methods

---
+++ value_object/magic_methods.py
@@ -0,0 +1,18 @@
+from .exceptions import FieldMutationAttempt
+
+def _eq(me, other):
+    if isinstance(other, me.__class__):
+        for field in me.__fields__:
+            if not (hasattr(other, field)
+                    and getattr(me, field) == getattr(other, field)):
+                return False
+        return True
+    else:
+        return NotImplemented
+
+def _ne(me, other):
+    result = _eq(me, other)
+    return result if result is NotImplemented else not result
+
+def _setattr(me, attr, value):
+    raise FieldMutationAttempt("Cannot modify field '%s'. ValueObject is immutable" % attr)
--- value_object/value_object.py
+++ value_object/value_object.py
@@ -1,4 +1,5 @@
 from .exceptions import *
+from .magic_methods import _eq, _ne, _setattr
 
 class ValueObject(type):
 
@@ -72,22 +73,3 @@ class ValueObject(type):
         for invariant in self.__invariants__:
             if not getattr(value_object, invariant)():
                 raise InvariantViolation("Fields %s violate invariant '%s'" % (self.__fields__, invariant))
-
-
-
-def _eq(me, other):
-    if isinstance(other, me.__class__):
-        for field in me.__fields__:
-            if not (hasattr(other, field)
-                    and getattr(me, field) == getattr(other, field)):
-                return False
-        return True
-    else:
-        return NotImplemented
-
-def _ne(me, other):
-    result = _eq(me, other)
-    return result if result is NotImplemented else not result
-
-def _setattr(me, attr, value):
-    raise FieldMutationAttempt("Cannot modify field '%s'. ValueObject is immutable" % attr)
